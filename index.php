b07dcf7918351933088a2ba153e9dd6083c35541
Gabe Chino <gabe.chino@gmail.com>
2017-12-07 14:28:20 -0700

    no message

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
 
-//header('Content-type: image/jpeg');
-//readfile('image.jpg');
+header('Content-type: image/jpeg');
+readfile('image.jpg');
 $filename = "logged.txt";
 $somecontent = "Connection from:".$_SERVER['REMOTE_ADDR']." at ".date("r",time())."\n";
 
 echo getcwd() . "\n";
 echo $somecontent . "\n";
 
-/*
    if ($handle = fopen($filename, 'a')) {
 	   fwrite($handle, $somecontent)
 	   fclose($handle);
    }
-*/
 
 ?>
