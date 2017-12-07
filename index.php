8eba59e0705dfd62d0b954e2f0a7dd175859a70b
Gabe Chino <gabe.chino@gmail.com>
2017-12-07 13:59:44 -0700

    no message

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 header('Content-type: image/jpeg');
 readfile('image.jpg');
 $filename = "logged.txt";
-   $somecontent = "Connection from:".$_SERVER['REMOTE_ADDR']." at ".date("r",time())."\n";
+/*   
+$somecontent = "Connection from:".$_SERVER['REMOTE_ADDR']." at ".date("r",time())."\n";
    if ($handle = fopen($filename, 'a')) {
 	   fwrite($handle, $somecontent)
 	   fclose($handle);
    }
-
+*/
 
 
 ?>
