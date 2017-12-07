313c6b639949c4aa53e235abe06911f30b976cd2
Gabe Chino <gabe.chino@gmail.com>
2017-12-07 13:41:07 -0700

    Added image

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-?>
+header('Content-type: image/jpeg');
+readfile('image.jpg');
+$filename = "logged.txt";
+   $somecontent = "Connection from:".$_SERVER['REMOTE_ADDR']." at ".date("r",time())."\n";
+   if ($handle = fopen($filename, 'a')) {
+	   fwrite($handle, $somecontent)
+	   fclose($handle);
+   }
 
-yoyo
+
+
+?>
